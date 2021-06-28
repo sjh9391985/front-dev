334a200dbd4262e444136d32ab76a0b5f1176061
JaeHyeon Son <sjh9391985@gmail.com>
2021-06-28 15:34:31 +0900

    #4 - Js Object

--- javascript-practices/ex07.js
+++ javascript-practices/ex07.js
@@ -11,3 +11,18 @@ MyObject.prototype.course = "douzone";
 MyObject.prototype.info = function(){
     console.log(this.name + " : " + this.age + " : " + this.school + " : " + this.course);
 }
+
+// MyObject 객체 생성 1
+var obj1 = new MyObject("둘리", 10);
+obj1.school = "multicampus" // 오버라이딩
+obj1.info();
+
+// MyObject 객체 생성 2
+var obj2 = new MyObject("마이콜", 20);
+obj2.info();
+
+// 오버라이딩
+obj2.info = function(){
+    console.log("비밀!!");
+}
+obj2.info();
