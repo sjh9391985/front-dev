27024e8e6034e5e528f85f4a1350d284f87f0de9
JaeHyeon Son <sjh9391985@gmail.com>
2021-07-02 14:52:23 +0900

    jQuery #Selector

--- ch07/src/main/webapp/jquery/jquery-simple.js
+++ ch07/src/main/webapp/jquery/jquery-simple.js
@@ -1,28 +1,34 @@
 // 간단 jquery 만들어 보기
-var jquery = function(param){
-	
-	if(typeof(param) === "function"){
+var jQuery = function(param) {
+	if(typeof(param) === 'function'){
 		window.addEventListener("load", param);
 		return;
 	}
 
-	if(typeof(param) === "string"){
-		var elements = document.querySelectorAll(selector);
-    	return new __jQuery(elements);
-		return null;
+	if(typeof(param) === 'string'){
+		var elements = document.querySelectorAll(param);
+		return new _jQuery(elements);
 	}
 }
 
-var __jQuery = function(elements){
+var _jQuery = function(elements) {
 	this.length = elements.length;
-
-	for(var i = 0; i<elements.length; i++){
-		this["" + i] = elements[i];
+	for(var i = 0; i < elements.length; i++){
+		this[""+i] = elements[i];
 	}
 }
 
-__jQuery.prototype.css = function(name, value){
-	for()
+
+_jQuery.prototype.css = function(name, value){
+	for(var i = 0; i < this.length; i++){
+		this[i].style[name] = value;
+	}
 }
 
-var $ = jquery;
+_jQuery.prototype.click = function(handler){
+	for(var i = 0; i < this.length; i++){
+		this[i].addEventListener("click", handler);
+	}
+}
+
+var $ = jQuery;
